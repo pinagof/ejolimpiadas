449ca91be91aa893ef3ee723c1bbcf400710eb70
pinagof <pinagof@ieselaios.com>
2026-03-12 15:23:37 +0000

    updaate rachamax

--- rachamax.cpp
+++ rachamax.cpp
@@ -4,6 +4,8 @@
 #include <sstream>
 using namespace std;
 
+int racha(vector<int> &vl, int largo);
+int datos(int n, vector<string> &rachas, vector<int> &vl);
 
 int main(){
     int n, a, resultado;
@@ -12,20 +14,42 @@ int main(){
     vector<int> vl;
     cout << "dime las rachas a analizar ";
     cin >> n;
-    while(cin.peek()=='\n'){cin.ignore();}
-    for (int i=0;i<n;i++){
-        getline(cin,temp);
-        rachas.push_back(temp);
-    }
+    datos(n,rachas,vl);
     for (int i=0;i<n;i++){
         istringstream ss(rachas[i]);
     vl.clear();
     while(ss >> a){vl.push_back(a);}
     if(vl.size()>0){
         resultado=racha(vl,vl.size());
-        cout << resultado <<"";
+        cout <<"" << resultado <<"";
         }else{cout<< "0";}
     }
 }
 
-int racha
+int datos(int n, vector<string> &rachas, vector<int> &vl){
+    string temp; 
+    while(cin.peek()=='\n'){cin.ignore();}
+    for (int i=0;i<n;i++){
+        getline(cin,temp);
+        rachas.push_back(temp);
+    }
+    return 0;
+}
+
+int racha(vector<int> &vl, int largo){
+    if (largo<1) {return 0;}
+    int mr=vl[0];
+    int sum=vl[0];
+    for(int i=1; i<largo; i++){
+        if(sum+vl[i]<vl[i]){
+            sum=vl[i];
+        }
+        else{
+            sum+=vl[i];
+        }
+    }
+    if(sum>mr){
+        mr=sum;
+    }
+    return mr; 
+}
