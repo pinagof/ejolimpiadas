228fbdd5229459c7f5b8ff50a8b28659a707ad5f
pinagof <pinagof@ieselaios.com>
2026-03-13 15:09:26 +0000

    update_filtrointer

--- filtrointer.cpp
+++ filtrointer.cpp
@@ -5,10 +5,11 @@
 using namespace std;
 
 int datos(int n, vector<string> &rachas, vector<int> &vl);
+string op(vector<int> &vl, int lg);
 
 int main(){
-    int n, a, resul;
-    string temp;
+    int n, a;
+    string temp, resul;
     vector<string> rachas;
     vector<int> vl;
     cout << "dime las rachas a analizar ";
@@ -16,17 +17,14 @@ int main(){
     datos(n,rachas,vl);
     for (int i=0;i<n;i++){
         istringstream ss(rachas[i]);
-    vl.clear();
-    while(ss >> a){vl.push_back(a);}
+        vl.clear();
+        while(ss >> a){vl.push_back(a);}
+        resul = op(vl, vl.size());
+        cout << resul << "\n";
     }
-    if (vl.size()==0){
-        cout << ""<<resul<<"\n";
-    }else { cout <<"No\n";}
-    
-
 }
 
-    int datos(int n, vector<string> &rachas, vector<int> &vl){
+int datos(int n, vector<string> &rachas, vector<int> &vl){
     string temp; 
     while(cin.peek()=='\n'){cin.ignore();}
     for (int i=0;i<n;i++){
@@ -36,18 +34,16 @@ int main(){
     return 0;
 }
 
-int datos(vector<int> &vl, int lg){
-    if (lg<1){return 0;}
+string op(vector<int> &vl, int lg){
+    if (lg<1){return "No";}
     int mj=vl[0];
     int sum=vl[0];
     for (int j=0; j<lg; j++){
         sum=0;
         for(int i=j; i<lg; i++){
             sum+=vl[i];
-            if (sum==0){return "Si"}
-
-
+            if (sum==0){return "Si";}
         }
     }
-
+    return "No";
 }
